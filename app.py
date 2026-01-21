3511522763e402db769065b10f95e3ddf817bbc3
SonuJaiswal6828 <jsonu77768@gmail.com>
2026-01-21 16:12:17 +0530

    Fixed config and logout route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, session
+from dotenv import load_dotenv
 import os
 #=========================================================================================================
 import pyrebase
